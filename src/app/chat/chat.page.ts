bc2e17618a1c0a5c3f4dcafbbbbae54e8067a68a
Maria Gabriela <mgvillarreal09@gmail.com>
2022-10-31 16:24:24 -0300

    agrego funciones chat

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+import { Mensaje } from '../clases/mensaje';
 import { ChatFirebaseService } from '../servicios/chat-firebase.service';
 
 @Component({
@@ -9,6 +11,10 @@ import { ChatFirebaseService } from '../servicios/chat-firebase.service';
 export class ChatPage implements OnInit {
 
   public mensaje:Array<any> = new Array<any>();
+  mensajeChat = new Mensaje;
+
+  nuevoMensaje:string = "";
+  usuarioLogueado:string;
 
   constructor(private firestoreApp: ChatFirebaseService) {
     firestoreApp.traerColeccion().subscribe(t=>
@@ -17,12 +23,39 @@ export class ChatPage implements OnInit {
         (<Array<any>>t).forEach(element =>
           this.mensaje.push(element)
         )
-        console.log("mensaje: ", this.mensaje);
+        console.log("Mensajes traidos: ", this.mensaje);
       }
     );
   }
 
   ngOnInit() {
+    this.obtenerUsuarioLogeado();
+  }
+
+  enviarMensaje():void{
+
+    //if(this.nuevoMensaje == "" ) return;
+
+    let datosUsuario = JSON.parse(localStorage.getItem('usuarioMobile'));
+    this.mensajeChat.usuario = datosUsuario.email;
+    this.mensajeChat.fecha = new Date;
+    this.mensajeChat.texto = this.nuevoMensaje;
+
+    console.log("Mensaje enviado: ", this.mensajeChat);
+
+    this.firestoreApp.setMensaje(JSON.parse(JSON.stringify(this.mensajeChat)));
+
+    this.nuevoMensaje = "";
+
+  }
+
+  obtenerUsuarioLogeado():void{
+    if(localStorage.getItem('usuarioMobile') !== null){
+      let datosUsuario = JSON.parse(localStorage.getItem('usuarioMobile'));
+      this.usuarioLogueado = datosUsuario.email;
+
+      console.log("Mail logueado chat mobile: ", this.usuarioLogueado);
+    }
   }
 
 }
--- src/app/servicios/chat-firebase.service.ts
+++ src/app/servicios/chat-firebase.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Mensaje } from '../clases/mensaje';
 
 @Injectable({
   providedIn: 'root'
@@ -12,4 +13,10 @@ export class ChatFirebaseService {
     var mensajes = this.firestore.collection('mensajes', ref=>ref.orderBy('fecha','asc'));
     return mensajes.valueChanges();
   }
+
+  setMensaje(mensajeNuevo: Mensaje){
+    var mensajeRef = this.firestore.collection('mensajes');
+    mensajeRef.add(mensajeNuevo);
+  }
+  
 }
